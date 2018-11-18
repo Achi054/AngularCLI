88f2c318c01ba5dce92fc85b7bdb1cbd43495e64
Sujith Acharya <sujith.acharya054@yahoo.com>
2018-11-18 19:56:35 +0530

    Build with stats

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { CustomerComponent } from "./customer/customer.component";
-import { AdminComponent } from "./admin/admin.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "dashboard" },
   { path: "dashboard", component: DashboardComponent },
-  { path: "customer", component: CustomerComponent }
+  { path: "customer", component: CustomerComponent },
 ];
 
 @NgModule({
